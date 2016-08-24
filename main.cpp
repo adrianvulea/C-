a8d5d2500178797ddfcb5497b43f54c3dbee50d5
adrianvulea <adrian.vulea@gmail.com>
2016-08-24 11:28:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*The program gets the name of a user, formulates a greeting and then
+incapsulates it into asterixes as such:
+                *******
+                *     *
+                * Adi *
+                *     *
+                *******
+*/
 #include <iostream>
 #include <string>
 
@@ -6,22 +14,23 @@ using namespace std;
 int main()
 {
 
-
+    // initialize name variable
     string name;
+    // escape character
     while ( name != "-1")
 
     {
+    
     cout << "Enter name, plz: ";
-
     cin >> name;
-
+    // formulating greeting
     const string greeting = "Hello, " + name + "!";
-
+    // building components of the output one piece at a time
     const string spaces(greeting.size(), ' ');
     const string second = "* " + spaces + " *";
 
     const string first(second.size(), '*');
-
+    // putting everything together
     cout << first << endl;
     cout << second << endl;
     cout << "* " + greeting + " *" << endl;
